a68cf773d58de068675f9d018cb8304d4015dabc
TonimaDristy <tidristy477@gmail.com>
2026-02-20 22:49:17 +0600

    Add  KeyFeatures

--- Frontend/src/components/features.tsx
+++ Frontend/src/components/features.tsx
@@ -5,15 +5,7 @@ import React from 'react';
 const KeyFeatures = () => {
   const features = [
     {
-      icon: "📂",
-      title: "Smart Resource Library",
-      description: "Access notes, slides, PDFs, and assignments categorized by:",
-      details: [
-        "University",
-        "Department", 
-        "Course",
-        "Semester",
-        "Resource Type"
+      
       ]
     },
     {
