57ea1ff3e9264a91e43d8e7f58172b8a7e3b4fc2
TonimaDristy <tidristy477@gmail.com>
2026-03-07 22:37:19 +0600

    function Hero

--- Frontend/src/components/hero.tsx
+++ Frontend/src/components/hero.tsx
@@ -1,9 +1,5 @@
 import Link from "next/link";
 
-export default function Hero() {
-    return (
-        <>
-        <div className="bg-white">
 
     <section className="bg-[#FCF8F1] bg-opacity-30 py-10 sm:py-16 lg:py-24">
         <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
