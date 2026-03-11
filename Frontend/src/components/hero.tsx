d231d4acfc456eaf7c8d91045f5bd34ca10278df
TonimaDristy <tidristy477@gmail.com>
2026-03-11 23:30:52 +0600

    inline-flex items

--- Frontend/src/components/hero.tsx
+++ Frontend/src/components/hero.tsx
@@ -19,11 +19,7 @@ export default function Hero() {
                 </p>
 
                 <Link
-                  href="/signup"
-                  title=""
-                  className="inline-flex items-center px-6 py-4 mt-8 font-semibold text-black transition-all duration-200 bg-yellow-300 rounded-full lg:mt-16 hover:bg-yellow-400 focus:bg-yellow-400"
-                  role="button"
-                >
+               
                   Join for free
                   <svg
                     className="w-6 h-6 ml-8 -mr-2"
