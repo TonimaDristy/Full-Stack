ef6a6b9d472ee3e16ba834ce83c7e287911469ca
TonimaDristy <tidristy477@gmail.com>
2026-03-13 22:48:21 +0600

    strokeLinejoin

--- Frontend/src/components/hero.tsx
+++ Frontend/src/components/hero.tsx
@@ -25,13 +25,14 @@ export default function Hero() {
                   role="button"
                 >
                   Join for free
-                  <svg
-                    className="w-6 h-6 ml-8 -mr-2"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  ></svg>
+                  <svg>
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="1.5"
+                      d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </svg>
                 </Link>
 
                 <p className="mt-5 text-gray-600">
